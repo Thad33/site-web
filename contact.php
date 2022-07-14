90352c22cf00bb240f50da329b3ca151172fd058
Thaddaeus <86629908+Thad33@users.noreply.github.com>
2022-07-14 17:07:15 +0300

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,80 @@
+<?php
+  
+if($_POST) {
+    $visitor_name = $_POST['visitorName'];
+    $visitor_email = $_POST['visitoremail'];
+    $concerned_department = $_POST ['concernedDepartment'];
+    $admission number = $_POST['admissionNumber'];
+    $coarse = $ POST['coarse'];
+//Database connection
+
+      $conn = new mysql('localhost','root','','Scooby');
+    if(isset($_POST['visitor_name'])) {
+        $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$visitor_name."</span>
+                        </div>";
+    }
+ 
+    if(isset($_POST['visitor_email'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$visitor_email."</span>
+                        </div>";
+    }
+      
+    if(isset($_POST['email_title'])) {
+        $email_title = filter_var($_POST['email_title'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$email_title."</span>
+                        </div>";
+    }
+      
+    if(isset($_POST['concerned_department'])) {
+        $concerned_department = filter_var($_POST['concerned_department'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Concerned Department:</b></label>&nbsp;<span>".$concerned_department."</span>
+                        </div>";
+    }
+      
+    if(isset($_POST['course'])) {
+        $visitor_message = htmlspecialchars($_POST['visitor_message']);
+        $email_body .= "<div>
+                           <label><b>Visitor Message:</b></label>
+                           <div>".$visitor_message."</div>
+                        </div>";
+    }
+      
+    if($concerned_department == "Nutrion") {
+        $recipient = "nutrtion@domain.com";
+    }
+    else if($concerned_department == "Pychology") {
+        $recipient = "pychology@domain.com";
+    }
+    else if($concerned_department == "Intensive care unit") {
+        $recipient = "intensive.care.unit@domain.com";
+    }
+    else if  ($concerned_department == "Intensive Care Medicine"){
+        $recipient = "intensive_care_edicine@domain.com";
+    }
+    else($concerned_department == "Clinical Medicine"){
+        $recipient = "clinical_medicine@domain.com";
+    }
+      
+    $email_body .= "</div>";
+ 
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $visitor_email . "\r\n";
+      
+    if(mail($recipient, $email_title, $email_body, $headers)) {
+        echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+      
+} else {
+    echo '<p>Something went wrong</p>';
+}
+?>
